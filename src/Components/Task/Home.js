605561350568e88061b437751e614e58979cc89d
monicakrishb <monica.krish@aspiresys.com>
2023-04-01 17:47:45 +0530

    modified with useremail

--- src/Components/Task/Home.js
+++ src/Components/Task/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/create.css";
 import { useNavigate } from "react-router-dom";
+import service from "../../services/API";
 
 export const Task = () => {
   useEffect(() => {
@@ -17,8 +18,7 @@ export const Task = () => {
   const navigate = useNavigate();
 
   const loadData = async () => {
-    const response = await axios
-      .get("http://localhost:8000/task")
+    const response = await service.homeget()
       .then((response) => {
         const filtered = response.data.map((val) => {
           const duedate = new Date(val.duedate);
@@ -59,14 +59,11 @@ export const Task = () => {
     return a.priority == "high";
   });
   console.log(highPriorityTasks);
-  function Delete(e) {
-    axios.delete("http://localhost:8000/task/" + e);
-    loadData();
-  }
+
+  const sessionuser = sessionStorage.getItem("useremail");
 
   return (
     <>
-      {/* <div className="highandcomplete"> */}
       <div className="center cTask">
         <h3>Create Your Task Here</h3>
         <div className="box addTask">
@@ -87,52 +84,64 @@ export const Task = () => {
           <h3>High Priority Task</h3>
 
           <div className="whole">
-            {highPriorityTasks.map((e) => (
-              <div
-                className="box"
-                style={{
-                  background:
-                    e.status == "inprogress"
-                      ? "orange"
-                      : e.status === "completed"
-                      ? "green"
-                      : "grey",
-                }}
-              >
-                <h4>
-                  <span>{e.taskname}</span> <br />
-                  <span>{e.description}</span> <br />
-                  <span>{e.duedate}</span>
-                  <br />
-                </h4>
-              </div>
-            ))}
+            {highPriorityTasks
+              .filter((e) => {
+                if (e.useremail == sessionuser) {
+                  return e;
+                }
+              })
+              .map((e) => (
+                <div
+                  className="box"
+                  style={{
+                    background:
+                      e.status == "inprogress"
+                        ? "orange"
+                        : e.status === "completed"
+                        ? "green"
+                        : "grey",
+                  }}
+                >
+                  <h4>
+                    <span>{e.taskname}</span> <br />
+                    <span>{e.description}</span> <br />
+                    <span>{e.duedate}</span>
+                    <br />
+                  </h4>
+                </div>
+              ))}
           </div>
         </div>
 
         <div className="task-container high" id="complete">
           <h3>Completed Task</h3>
           <div className="whole">
-            {completedTask.map((e) => (
-              <div
-                className="box"
-                style={{
-                  background:
-                    e.status == "inprogress"
-                      ? "orange"
-                      : e.status === "completed"
-                      ? "green"
-                      : "grey",
-                }}
-              >
-                <h4>
-                  <span>{e.taskname}</span> <br />
-                  <span>{e.description}</span> <br />
-                  <span>{e.priority}</span> <br />
-                  <span>{e.duedate}</span> <br />
-                </h4>
-              </div>
-            ))}
+            {completedTask
+              .filter((e) => {
+                if (e.useremail == sessionuser) {
+                  return e;
+                }
+              })
+              .map((e) => (
+                <div
+                  className="box"
+                  style={{
+                    background:
+                      e.status == "inprogress"
+                        ? "orange"
+                        : e.status === "completed"
+                        ? "green"
+                        : "grey",
+                  }}
+                >
+                  <h4>
+                    <span>{e.taskname}</span> <br />
+                    <span>{e.description}</span> <br />
+                    <span>{e.priority}</span> <br />
+                    <span>{e.duedate}</span> <br />
+                  </h4>
+                </div>
+              ))}
           </div>
         </div>
       </div>
@@ -156,7 +165,11 @@ export const Task = () => {
         <div className="task-container">
           {userdata &&
             userdata
-              .filter((e) => e.priority == priority)
+              .filter((e) => {
+                if (e.useremail == sessionuser && e.priority == priority) {
+                  return e;
+                }
+              })
               .map((e) => (
                 <div className="whole">
                   <div
@@ -179,155 +192,6 @@ export const Task = () => {
                   </div>
                 </div>
               ))}
-        </div>
-      </div>
-
-      <h1>List Of Tasks</h1>
-      <div className="partsec">
-        {/* <div className="center">
-
-        
-        <h1>Create Your Task Here</h1>
-        <div className="box addTask">
-          <span
-            class="material-symbols-outlined"
-            onClick={() => {
-              navigate("/create");
-            }}
-          >
-            add
-          </span>{" "}
-          <h4 id="add">Add a task</h4>
-        </div>
-</div> */}
-        <div className="container">
-          {/* <div
-          
-          >
-            <tr>
-              <th>Task Name</th>
-              <th>Description</th>
-              <th>Priority</th>
-              <th>Duedate</th>
-              <th>Remaining Days</th>
-              <th>Status</th>
-              <th>Action</th>
-              <th>Action</th>
-            </tr>
-
-            {userdata.map((e) => (
-              <tr key={e.taskname}>
-                <td
-                  className="taskname"
-                  style={{
-                    color:
-                      e.status == "inprogress"
-                        ? "orange"
-                        : e.status === "completed"
-                        ? "green"
-                        : "grey",
-                  }}
-                >
-                  {e.taskname}
-                </td>
-                <td>{e.description}</td>
-                <td>{e.priority}</td>
-                <td>{e.duedate} </td>
-                <td>{e.remaining}</td>
-                <td>{e.status}</td>
-                <td>
-                  <button
-                    id="edit"
-                    className="editbutton"
-                    onClick={() => {
-                      navigate("edit/" + e.id);
-                    }}
-                  >
-                    Edit
-                  </button>
-                </td>
-                <td>
-                  <button
-                    id="edit"
-                    className="editbutton"
-                    onClick={() => {
-                      Delete(e.id);
-                    }}
-                  >
-                    {" "}
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </div> */}
-          <div className="listoftasks">
-            {userdata.map((e) => (
-              <div
-                className="allTask"
-                style={{
-                  background:
-                    e.status == "inprogress"
-                      ? "orange"
-                      : e.status === "completed"
-                      ? "green"
-                      : "grey",
-                }}
-              >
-                <span key={e.taskname}>
-                  {" "}
-                  <br />
-                  <span
-                    className="taskname"
-                    // style={{
-                    //   color:
-                    //     e.status == "inprogress"
-                    //       ? "orange"
-                    //       : e.status === "completed"
-                    //       ? "green"
-                    //       : "grey",
-                    // }}
-                  >
-                    Task Name :{e.taskname}
-                  </span>
-                  <br />
-                  <span>Description :{e.description}</span>
-                  <br />
-                  <span> Priority :{e.priority}</span>
-                  <br />
-                  <span>Duedate :{e.duedate} </span>
-                  <br />
-                  <span>Remaining Days :{e.remaining}</span>
-                  <br />
-                  <span>Status :{e.status}</span>
-                  <br />
-                  <span>
-                    <span
-                      id="edit"
-                      className="editbutton material-symbols-outlined"
-                      onClick={() => {
-                        navigate("edit/" + e.id);
-                      }}
-                    >
-                      Edit
-                    </span>
-                  </span>
-                  <span>
-                    <span
-                      id="edit"
-                      className="editbutton  material-symbols-outlined"
-                      onClick={() => {
-                        Delete(e.id);
-                      }}
-                    >
-                      {" "}
-                      Delete
-                    </span>
-                  </span>
-                </span>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </>

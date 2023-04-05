ba794ede550aff95c86f8876b10c1ca8a01bc889
monicakrishb <monica.krish@aspiresys.com>
2023-04-05 17:44:08 +0530

    added api

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+
 class HttpService {
   environment = process.env.REACT_APP_ENV;
-  registerpost = async (obj) => {
-    return await axios.post(this.environment + "/user", obj);
+  registerpost = (obj) => {
+    return axios.post(this.environment + "/user", obj);
   };
   loginget = (email) => {
     return axios.get(this.environment + `/user?email=${email}`);
@@ -21,5 +22,9 @@ class HttpService {
   };
 }
 
+export const passvalid = (pass) => {
+  const passregex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passregex.test(pass);
+};
 const service = new HttpService();
 export default service;

9b7898368fa2d26a80212609b7e4d7d202de0b06
monicakrishb <monica.krish@aspiresys.com>
2023-04-10 17:11:07 +0530

    wrote testcases

---
+++ src/Components/test/logout.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Logout from "../form/Logout";
+
+test("Logout", async () => {
+  render(
+    <MemoryRouter>
+      <Logout />
+    </MemoryRouter>
+  );
+});

cdbdf34374c5806a87398847732d51ca58e52d7f
monicakrishb <monica.krish@aspiresys.com>
2023-04-10 17:12:13 +0530

    wrote test cases

--- src/Components/test/Register.test.js
+++ src/Components/test/Register.test.js
@@ -1,6 +1,9 @@
-import { render} from '@testing-library/react';
-import { Register } from '../form/Register';
-import { MemoryRouter } from 'react-router-dom';
+import { act, render, screen } from "@testing-library/react";
+import { Register } from "../form/Register";
+import { MemoryRouter } from "react-router-dom";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { fireEvent } from "@testing-library/react";
 
 
 const mockedUsedNavigate = jest.fn();
@@ -8,7 +11,58 @@ jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockedUsedNavigate,
 }));
-test('Register', async() => {
+test("Register", async () => {
   render(
-  <MemoryRouter><Register/></MemoryRouter>);
+    <MemoryRouter>
+      <Register />
+    </MemoryRouter>
+  );
+});
+const user = [
+  {
+    name: "Kuralanban",
+    email: "kural@gmail.com",
+    pass: "Kural",
+    mobile: "9003759065",
+    id: 3,
+  },
+  {
+    name: "Monica",
+    email: "monica@gmail.com",
+    pass: "Moni@13",
+    mobile: "9003759065",
+    id: 4,
+  },
+];
+test("async check registerpost", async () => {
+  const mock = new MockAdapter(axios);
+  mock.onPost("http://localhost:8000/user").reply(200, [{}]);
+  render(
+    <MemoryRouter>
+      <Register />
+    </MemoryRouter>
+  );
+  await act(() => {});
+  screen.getAllByTestId("clickme")[0].click();
+  await act(() => {});
+});
+test("renders one button", async () => {
+  render(
+    <MemoryRouter>
+      <Register />
+    </MemoryRouter>
+  );
+  const items = await screen.findAllByRole("button");
+  expect(items).toHaveLength(2);
+});
+test("test username input is valid", () => {
+  render(
+    <MemoryRouter>
+      <Register />
+
+    </MemoryRouter>
+  );
+  const inputElement = screen.getByTestId("username-test");
+  fireEvent.change(inputElement, { target: { value: "Monica" } });
+  expect(screen.getByTestId("username-test")).toHaveValue("Monica");
 });

5eccb00260f23b60a4dcc37bd7bb744a664e6214
monicakrishb <monica.krish@aspiresys.com>
2023-04-13 17:19:13 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Task } from "./Components/Task/Home";
-import { Edit } from "./Components/Task/Edit";
-import {Register} from "./Components/form/Register"
+import { Register } from "./Components/form/Register";
 import { Login } from "./Components/form/Login";
 import Nav from "./Components/form/Navbar";
 import { Create } from "./Components/Task/Create";
 import { Tasklist } from "./Components/Task/Tasklist";
 import ProtectUser from "./Components/ProtectedRoute/Protect";
 import { ToastContainer } from "react-toastify";
+import { createContext, useState } from "react";
 
+export const EditContext = createContext({});
 
 function App() {
+  const [task, setTask] = useState();
   return (
     <div className="App">
       <Nav />
       <ToastContainer theme="colored"></ToastContainer>
-      <Routes>
-        <Route path="/register" element={<Register/>} />
+      <Routes >
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-    
+
         <Route element={<ProtectUser />}>
-          <Route path="/tasklist" element={<Tasklist />} />
+          <Route path="/tasklist" element={<Tasklist setTask={setTask} />} />
           <Route path="/task" element={<Task />} />
-          <Route path="/task/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create data={task} />} />
           <Route path="/create" element={<Create />} />
         </Route>
       </Routes>

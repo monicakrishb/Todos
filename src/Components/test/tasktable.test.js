21b84c63f48b313c3b6a6bf66aaf198db635fb43
monicakrishb <monica.krish@aspiresys.com>
2023-04-18 17:11:09 +0530

    rendered the component

---
+++ src/Components/test/tasktable.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import TaskTable from "../Task/TaskTable";
+
+test("test app", () => {
+  render(
+    <MemoryRouter>
+      <TaskTable />
+    </MemoryRouter>
+  );
+});

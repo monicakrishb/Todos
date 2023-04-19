96d4219ec35943f723af783c2c243421b04ec889
monicakrishb <monica.krish@aspiresys.com>
2023-04-19 17:06:12 +0530

    modified routing

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import { Tasklist } from "./Components/Task/Tasklist";
 import ProtectUser from "./Components/ProtectedRoute/Protect";
 import { ToastContainer } from "react-toastify";
 import { createContext, useState } from "react";
-import TaskTable from "./Components/Task/TaskTable";
 import Home from "./Components/Task/Home";
 
 export const EditContext = createContext({});
 
 function App() {
   const [task, setTask] = useState();
+
   return (
     <div className="App">
       <Nav />
-      <ToastContainer></ToastContainer>
+      <ToastContainer />
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
@@ -26,9 +26,10 @@ function App() {
         <Route element={<ProtectUser />}>
           <Route path="/tasklist" element={<Tasklist setTask={setTask} />} />
           <Route path="/task" element={<Home />} />
-          <Route path="/create" element={<Create data={task} />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/table" element={<TaskTable />} />
+          <Route
+            path="/create"
+            element={<Create data={task} setTask={setTask} />}
+          />
         </Route>
       </Routes>
     </div>

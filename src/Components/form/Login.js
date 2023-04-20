3f0869b32377971f13f0632b4f238e044ef1270a
monicakrishb <monica.krish@aspiresys.com>
2023-04-20 14:45:50 +0530

    modified in html

--- src/Components/form/Login.js
+++ src/Components/form/Login.js
@@ -1,63 +1,52 @@
 import React, { useEffect, useState } from "react";
-import service from "../../services/API";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import service from "../../services/API";
 import "../styles/form.css";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-  const usenavigate = useNavigate();
-  const validate = (email, pass) => {
-    let result = true;
-    if (email === "" || email === null) {
-      result = false;
-    }
-    if (pass === "" || pass === null) {
-      result = false;
-    }
-    return result;
-  };
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (sessionStorage.getItem("useremail")) {
-      usenavigate("/task");
+      navigate("/task");
     }
-  }, []);
+  }, [navigate]);
 
-  const ProceedLogin = async (e) => {
+  const validate = (email, pass) => {
+    return email.trim() !== "" && pass.trim() !== "";
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (validate(email, pass)) {
       try {
-        await service.loginget(email).then((response) => {
-          if (Object.keys(response.data).length === 0) {
-            toast.error("Please enter valid useremail");
-          } else {
-            if (response.data[0].pass === pass) {
-              toast.success(" login success");
-
-              sessionStorage.setItem("useremail", email);
-
-              usenavigate("/task");
-            } else {
-              if (response.data[0].pass != pass) {
-                toast.error("Please enter valid password");
-              }
-            }
+        const response = await service.loginget(email);
+        if (Object.keys(response.data).length === 0) {
+          toast.error("Please enter valid user email");
+        } else {
+          if (response.data[0].pass === pass) {
+            toast.success("Login success");
+            sessionStorage.setItem("useremail", email);
+            navigate("/task");
+          } else if (response.data[0].pass !== pass) {
+            toast.error("Please enter valid password");
           }
-        });
+        }
       } catch (err) {
-        toast.error("Login Failed due to:" + err.message);
+        toast.error("Login Failed due to: " + err.message);
       }
     } else {
-      toast.warning("Please enter useremail and password");
+      toast.warning("Please enter user email and password");
     }
   };
 
   return (
     <div className="auth-form-container form">
       <h2>Login</h2>
-      <form className="login-form" onSubmit={ProceedLogin}>
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input
           value={email}
@@ -67,6 +56,7 @@ export const Login = () => {
           data-testid="username-test"
           id="email"
           name="email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
         />
         <label htmlFor="password">Password</label>
         <input
@@ -76,11 +66,15 @@ export const Login = () => {
           placeholder="Enter Password"
           id="password"
           name="password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
         />
-        <button type="submit" data-testid="submitme">Log In</button>
+        <button type="submit" data-testid="submitme">
+          Log In
+        </button>
       </form>
       <a href="/register" id="logindesc">
-        <button className="link-btn" >
+        <button className="link-btn">
           Don't have an account? Register here.
         </button>
       </a>

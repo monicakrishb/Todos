bbdf0f2ff162bb91925e21df4fca807076e2ad5d
monicakrishb <monica.krish@aspiresys.com>
2023-04-20 14:39:50 +0530

    modified heading tag

--- src/Components/Task/Home.js
+++ src/Components/Task/Home.js
@@ -3,33 +3,32 @@ import TaskTable from "./TaskTable";
 import service from "../../services/API";
 
 function Home() {
-  useEffect(() => {
-    loadData();
-  }, []);
-
   const [userdata, setUserdata] = useState([]);
   const [priority, setPriority] = useState("");
 
   const loadData = async () => {
-    const response = await service.homeget().then((response) => {
-      const filtered = response.data.map((val) => {
-        const duedate = new Date(val.duedate);
-        const currentDate = new Date();
-        const dueDateTime = duedate.getTime();
-        const diff = dueDateTime - currentDate.getTime();
-        val.remaining = Math.round(diff / (1000 * 60 * 60 * 24));
-        return val;
-      });
-      setUserdata(filtered);
+    const response = await service.homeget();
+    const filtered = response.data.map((val) => {
+      const duedate = new Date(val.duedate);
+      const currentDate = new Date();
+      const dueDateTime = duedate.getTime();
+      const diff = dueDateTime - currentDate.getTime();
+      val.remaining = Math.round(diff / (1000 * 60 * 60 * 24));
+      return val;
     });
+    setUserdata(filtered);
   };
 
+  useEffect(() => {
+    loadData();
+  }, []);
+
   const completedTask = userdata.filter((a) => {
-    return a.status == "completed";
+    return a.status === "completed";
   });
 
   const highPriorityTasks = userdata.filter((a) => {
-    return a.priority == "high";
+    return a.priority === "high";
   });
 
   const sessionuser = sessionStorage.getItem("useremail");
@@ -37,27 +36,41 @@ function Home() {
   return (
     <div className="routes">
       <div className="center cTask"></div>
-      <h3>High Priority Task</h3>
+      <h2 id="high-priority-tasks">High Priority Task</h2>
 
       <TaskTable
         tasks={highPriorityTasks}
         sessionuser={sessionuser}
         priority="high"
-        headers={["Task Name", "Description", "Due Date", "Remaining Days", "Status"]}
+        headers={[
+          "Task Name",
+          "Description",
+          "Due Date",
+          "Remaining Days",
+          "Status",
+        ]}
       />
-      <h3>Completed Task</h3>
+      <h2 id="completed-tasks">Completed Task</h2>
 
       <div>
         <TaskTable
           tasks={completedTask}
           sessionuser={sessionuser}
           status="completed"
-          headers={["Task Name", "Description", "Due Date", "Remaining Days", "Status"]}
+          headers={[
+            "Task Name",
+            "Description",
+            "Due Date",
+            "Remaining Days",
+            "Status",
+          ]}
         />
       </div>
 
       <div>
-        <h3 style={{ color: "black" }}>Categorize Your Tasks Here!</h3>
+        <h2 style={{ color: "black" }} id="categorize-tasks">
+          Categorize Your Tasks Here!
+        </h2>
 
         <select
           className="form-control category"
@@ -66,7 +79,7 @@ function Home() {
           value={priority}
           onChange={(e) => setPriority(e.target.value)}
         >
-          <option>Pending</option>
+          <option value="">Pending</option>
           <option value="high">High</option>
           <option value="medium">Medium</option>
           <option value="low">Low</option>
@@ -77,7 +90,13 @@ function Home() {
         tasks={userdata}
         sessionuser={sessionuser}
         priority={priority}
-        headers={["Task Name", "Description", "Due Date", "Remaining Days", "Status"]}
+        headers={[
+          "Task Name",
+          "Description",
+          "Due Date",
+          "Remaining Days",
+          "Status",
+        ]}
       />
     </div>
   );

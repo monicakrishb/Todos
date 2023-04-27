b36166710983b9b3c302acf88ac8703dfddd9e5a
monicakrishb <monica.krish@aspiresys.com>
2023-04-27 17:05:58 +0530

    inserted piechart

--- src/Components/Task/Tasklist.jsx
+++ src/Components/Task/Tasklist.jsx
@@ -1,12 +1,210 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import service from "../../services/API";
 import "../styles/create.css";
+import * as d3 from "d3";
+import { pie } from "d3";
 
 export const Tasklist = ({ setTask }) => {
   const [userdata, setUserdata] = useState([]);
   const navigate = useNavigate();
   const sessionuser = sessionStorage.getItem("useremail");
+  const [data, setData] = useState([
+    {
+      property: "Completed",
+      value: 0,
+      weeklyCompleted: 0,
+      monthlyCompleted: 0,
+    },
+
+    { property: "In Progress", value: 0 },
+    { property: "Cancelled", value: 0 },
+    { property: "Pending", value: 0 },
+  ]);
+  const svgRef = useRef();
+  const WeeklycompletedDate = [];
+  const monthlycompletedDate = [];
+
+  const WeeklyinprogressDate = [];
+  const monthlyinprogressDate = [];
+
+  const Weeklycancelled = [];
+  const monthlycancelled = [];
+
+  const Weeklypending = [];
+  const monthlypending = [];
+
+  const today = new Date();
+  const startOfWeek = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - today.getDay()
+  );
+  const endOfWeek = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + (6 - today.getDay())
+  );
+  const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+  useEffect(() => {
+    const completedCount = userdata.filter((a) => {
+      if (
+        a.status === "completed" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      ) {
+        console.log(a.duedate);
+
+        if (
+          new Date(a.duedate) >= startOfWeek &&
+          new Date(a.duedate) <= endOfWeek
+        ) {
+          console.log("weekly");
+          WeeklycompletedDate.push(a.duedate);
+          console.log(WeeklycompletedDate.length);
+        } else if (
+          new Date(a.duedate) >= startOfMonth &&
+          new Date(a.duedate) <= endOfMonth
+        ) {
+          console.log("monthly");
+          monthlycompletedDate.push(a.duedate);
+        }
+      }
+      return (
+        a.status === "completed" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      );
+    }).length;
+    console.log(completedCount);
+    const inProgressCount = userdata.filter((a) => {
+      if (
+        a.status === "inprogress" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      ) {
+        console.log(a.duedate);
+        WeeklyinprogressDate.push(a.duedate);
+        console.log(WeeklyinprogressDate);
+
+        if (
+          new Date(a.duedate) >= startOfWeek &&
+          new Date(a.duedate) <= endOfWeek
+        ) {
+          WeeklyinprogressDate.push(a.duedate);
+        } else if (
+          new Date(a.duedate) >= startOfMonth &&
+          new Date(a.duedate) <= endOfMonth
+        ) {
+          monthlyinprogressDate.push(a.duedate);
+        }
+      }
+      return (
+        a.status === "inprogress" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      );
+    }).length;
+    const cancelledCount = userdata.filter((a) => {
+      if (
+        a.status === "cancelled" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      ) {
+        console.log(a.duedate);
+        console.log(Weeklycancelled);
+        if (
+          new Date(a.duedate) >= startOfWeek &&
+          new Date(a.duedate) <= endOfWeek
+        ) {
+          Weeklycancelled.push(a.duedate);
+        } else if (
+          new Date(a.duedate) >= startOfMonth &&
+          new Date(a.duedate) <= endOfMonth
+        ) {
+          monthlycancelled.push(a.duedate);
+        }
+      }
+      return (
+        a.status === "cancelled" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      );
+    }).length;
+    const pendingCount = userdata.filter((a) => {
+      if (
+        a.status === "cancelled" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      ) {
+        console.log(a.duedate);
+        console.log(Weeklypending);
+        if (
+          new Date(a.duedate) >= startOfWeek &&
+          new Date(a.duedate) <= endOfWeek
+        ) {
+          Weeklypending.push(a.duedate);
+        } else if (
+          new Date(a.duedate) >= startOfMonth &&
+          new Date(a.duedate) <= endOfMonth
+        ) {
+          monthlypending.push(a.duedate);
+        }
+      }
+      return (
+        a.status === "pending" &&
+        a.useremail == sessionStorage.getItem("useremail")
+      );
+    }).length;
+
+    setData([
+      {
+        property: completedCount == 0 ? "" : `Completed ${completedCount}%`,
+        value: completedCount,
+        weeklyCompleted: WeeklycompletedDate.length,
+        monthlyCompleted: monthlycompletedDate.length,
+      },
+      {
+        property: inProgressCount == 0 ? "" : `In Progress ${inProgressCount}%`,
+        value: inProgressCount,
+      },
+      {
+        property: cancelledCount == 0 ? "" : `Cancelled ${cancelledCount}%`,
+        value: cancelledCount,
+      },
+      {
+        property: pendingCount == 0 ? "" : `Pending ${pendingCount}%`,
+        value: pendingCount,
+      },
+    ]);
+  }, [userdata]);
+
+  useEffect(() => {
+    const w = 500;
+    const h = 500;
+    const radius = w / 3;
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", w)
+      .attr("height", h)
+      .style("overflow", "visible");
+    const formattedData = pie().value((d) => d.value)(data);
+    const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
+    const color = d3.scaleOrdinal().range(d3.schemeSet2);
+
+    svg
+      .selectAll()
+      .data(formattedData)
+      .join("path")
+      .attr("d", arcGenerator)
+      .attr("fill", (d) => color(d.data.property))
+      .style("opacity", 1);
+
+    svg
+      .selectAll()
+      .data(formattedData)
+      .join("text")
+      .text((d) => d.data.property)
+      .attr("transform", (d) => `translate(${arcGenerator.centroid(d)})`)
+      .style("text-anchor", "middle")
+      .style("font-size", "16px")
+      .style("fill", "white");
+  }, [data]);
 
   useEffect(() => {
     loadData();
@@ -33,7 +231,6 @@ export const Tasklist = ({ setTask }) => {
     "Remaining Days",
     "Status",
     "Action",
-    "Action",
   ];
 
   async function Delete(id) {
@@ -45,9 +242,26 @@ export const Tasklist = ({ setTask }) => {
     }
   }
 
+  console.log(WeeklycompletedDate);
+  console.log(monthlycompletedDate);
   return (
     <>
       <div className="routes">
+        <div id="pie">
+          <svg ref={svgRef} id="ref"></svg>
+          <table>
+            <tr>
+              <th>Performance</th>
+              <th>Weekly count</th>
+              <th>Monthly count</th>
+            </tr>
+            <tr>
+              <td>Completed</td>
+              <td>{data[0].weeklyCompleted}</td>
+              <td>{data[0].monthlyCompleted}</td>
+            </tr>
+          </table>
+        </div>
         <div className="center cTask"></div>
         <h2 id="tasklist">List Of Tasks</h2>
 
@@ -89,7 +303,7 @@ export const Tasklist = ({ setTask }) => {
                   <td>{e.duedate} </td>
                   <td>{e.remaining}</td>
                   <td>{e.status}</td>
-                  <td>
+                  <td className="actionBtns">
                     <button
                       id="edit"
                       className="editbutton material-symbols-outlined"
@@ -100,8 +314,6 @@ export const Tasklist = ({ setTask }) => {
                     >
                       Edit
                     </button>
-                  </td>
-                  <td className="tdata">
                     <button
                       id="edit"
                       className="editbutton  material-symbols-outlined"
@@ -112,6 +324,17 @@ export const Tasklist = ({ setTask }) => {
                       Delete
                     </button>
                   </td>
+                  {/* <td className="tdata">
+                    <button
+                      id="edit"
+                      className="editbutton  material-symbols-outlined"
+                      onClick={() => {
+                        Delete(e.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td> */}
                 </tr>
               ))}
           </tbody>
